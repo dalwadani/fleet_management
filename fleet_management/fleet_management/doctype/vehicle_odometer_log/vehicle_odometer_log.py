6d592a52bc315b037ee4c5847b4e8618de01d8b8
Dhaifallah Alwadani <dalwadani@gmail.com>
2014-08-23 03:49:13 +0100

    Update Vehicle odometer with new log

--- fleet_management/fleet_management/doctype/vehicle_odometer_log/vehicle_odometer_log.py
+++ fleet_management/fleet_management/doctype/vehicle_odometer_log/vehicle_odometer_log.py
@@ -6,5 +6,12 @@ import frappe
 from frappe.model.document import Document
 
 class VehicleOdometerLog(Document):
-        def validate(self):
-                self.odometer_reading
+        def on_submit(self):
+                last_reading = frappe.get_list("Vehicle",
+                                               fields=["odometer_reading", "reading_time"],
+                                               filters = {
+                                                       "name": self.vehicle
+                                               })
+                if (self.reading_time >= last_reading[0].reading_time):
+                        frappe.set_value("Vehicle",self.vehicle,"odometer_reading", self.odometer_reading)
+                        frappe.set_value("Vehicle",self.vehicle,"reading_time", self.reading_time)
