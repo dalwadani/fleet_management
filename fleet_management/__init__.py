702952ae61454b12fc269aa562ced79878edf776
Dhaifallah Alwadani <dalwadani@gmail.com>
2014-08-25 14:08:02 +0100

    creat roles with setup

---
+++ fleet_management/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+install_docs = [
+    {'doctype':'Role', 'role_name':'Fleet Manager', 'name':'Fleet Manager'},
+    {'doctype':'Role', 'role_name':'Fleet User', 'name':'Fleet User'}
+]
